532d2c6b876cb38db2e014017a87140b8d7ed9a8
Clark Ritchie <clark.ritchie@blueboard.com>
2023-04-20 10:56:35 -0700

    cleanup

--- prestart.sh
+++ prestart.sh
@@ -2,3 +2,13 @@
 
 echo "Sleeping for 10 seconds to allow Maria DB to start..."
 sleep 10;
+
+echo "Setting alembic connection string to use env vars for sqlalchemy.url..."
+
+INI_FILE="alembic.ini"
+export CONN="sqlalchemy.url = mysql+pymysql\:\/\/${MYSQL_USER}\:${MYSQL_PASSWORD}\@${MYSQL_HOSTNAME}\:${MYSQL_PORT}\/${MYSQL_DATABASE}"
+
+# back up original ini file for good measure
+cp ${INI_FILE} ${INI_FILE}.bak
+# replace the connection string with the new connection string
+sed -i "s/sqlalchemy.url.*/$CONN/" ${INI_FILE}
